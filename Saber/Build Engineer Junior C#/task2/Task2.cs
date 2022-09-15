9ee1acfb5584dcc97700df2a1f608e76383dfd8b
Syslop <petrovkonstantined@gmail.com>
2022-09-15 14:07:16 +0300

    Update file task 2. Added method RemoveDupsWithoutRemoveMethod. Rename method RemoveDupsWithRemoveMethod.

--- Saber/Build Engineer Junior C#/task2/Task2.cs
+++ Saber/Build Engineer Junior C#/task2/Task2.cs
@@ -10,19 +10,35 @@ Console.WriteLine(data); // "A B A"
 */
 using System.Text;
 
-var data = new StringBuilder("AAA BBB AAA");
+//Input block
+var data = new StringBuilder("AAA ABBB AAA");
 
 Console.WriteLine(data);
-RemoveDups(data);
+RemoveDupsWithRemoveMethod(data);
+//RemoveDupsWithoutRemoveMethod(data);
 Console.WriteLine(data);
 
-void RemoveDups(StringBuilder str) 
+//RemoveDups with using method Remove()
+void RemoveDupsWithRemoveMethod(StringBuilder str) 
 {
     for (int i = 1; i < str.Length; i++)
     {
         if (str[i] == str[i - 1])
         {
             str.Remove(i, 1);
+            i--;
+        }
+    }
+}
+
+//RemoveDups without using method Remove()
+void RemoveDupsWithoutRemoveMethod(StringBuilder str) 
+{
+    for (int i = 1; i < str.Length; i++)
+    {
+        if (str[i] == str[i - 1])
+        {
+            str.Insert(i, "");
             i--;
         }
     }
