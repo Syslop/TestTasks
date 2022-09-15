58a66f44d725d2592f5aefdd12f87d7753fe8ce3
Syslop <petrovkonstantined@gmail.com>
2022-09-15 13:59:12 +0300

    Create file task 2. Added method RemoveDups with SB.Remove().

---
+++ Saber/Build Engineer Junior C#/task2/Task2.cs
@@ -0,0 +1,29 @@
+﻿/*Task 2
+
+2.	Напишите функцию, удаляющую последовательно дублирующие символы в строке:
+
+// пример использования
+var data = new StringBuilder("AAA BBB AAA");
+RemoveDups(data);
+Console.WriteLine(data); // "A B A"
+
+*/
+using System.Text;
+
+var data = new StringBuilder("AAA BBB AAA");
+
+Console.WriteLine(data);
+RemoveDups(data);
+Console.WriteLine(data);
+
+void RemoveDups(StringBuilder str) 
+{
+    for (int i = 1; i < str.Length; i++)
+    {
+        if (str[i] == str[i - 1])
+        {
+            str.Remove(i, 1);
+            i--;
+        }
+    }
+}
