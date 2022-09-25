cad5ab81aaa33985bd51d72a221542d49f62b6c0
Syslop <petrovkonstantined@gmail.com>
2022-09-25 16:29:03 +0300

    Create project with dependencies

---
+++ EVRIKA/Junior Java fullstack developer/usersRegistrationSinglePageApp/src/main/java/com/example/usersRegistrationSinglePageApp/UsersRegistrationSinglePageAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersRegistrationSinglePageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersRegistrationSinglePageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersRegistrationSinglePageAppApplication.class, args);
+	}
+
+}
