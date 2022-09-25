4fe089a1120ef364f9b6e0974de5686e931abaf7
Syslop <petrovkonstantined@gmail.com>
2022-09-25 16:18:28 +0300

    Create project

---
+++ EVRIKA/usersRegistrationSinglePageApp/src/test/java/com/example/usersRegistrationSinglePageApp/UsersRegistrationSinglePageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersRegistrationSinglePageApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersRegistrationSinglePageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
